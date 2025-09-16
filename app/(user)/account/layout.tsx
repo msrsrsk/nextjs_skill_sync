f2ce16761579fc4ac37d2684bdcca83f1d08ce67
Naoko Kitahara <suisai.port@gmail.com>
2025-09-16 17:57:30 +0900

    [Test]

--- app/(user)/account/layout.tsx
+++ app/(user)/account/layout.tsx
@@ -1,2 +1,8 @@
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic"
 export const revalidate = 0
+
+export default function AccountLayout({
+    children,
+}: { children: React.ReactNode }) {
+    return <>{children}</>
+}
