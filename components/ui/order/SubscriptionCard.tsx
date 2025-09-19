4b17d942dfb6ae61a01f8bc6bd48e12120a006cc
Naoko Kitahara <suisai.port@gmail.com>
2025-09-19 21:44:44 +0900

    [Fix] サブスク一覧の支払日項目の修正

--- components/ui/order/SubscriptionCard.tsx
+++ components/ui/order/SubscriptionCard.tsx
@@ -2,18 +2,24 @@ import OrderItemList from "@/components/ui/order/OrderItemList"
 import { LinkButtonPrimary } from "@/components/common/buttons/Button"
 import { formatDate } from "@/lib/utils/format"
 import { MoreIcon } from "@/components/common/icons/SvgIcons"
-import { SITE_MAP } from "@/constants/index"
+import { SUBSCRIPTION_PAYMENT_STATUS, SITE_MAP } from "@/constants/index"
 
+const { CANCELED } = SUBSCRIPTION_PAYMENT_STATUS;
 const { SUBSCRIPTION_HISTORY_PATH } = SITE_MAP;
 
 const SubscriptionCard = async ({ orderItem }: { orderItem: OrderItem }) => {
     const {
         order_id,
-        subscription_next_payment
+        subscription_next_payment,
+        subscription_status
     } = orderItem;
 
-    const formattedNextPaymentDate = subscription_next_payment 
-        ? formatDate(subscription_next_payment) : 'No data';
+    const formattedNextPaymentDate = subscription_status !== CANCELED 
+        ? subscription_next_payment 
+        ? formatDate(subscription_next_payment) 
+        : 'No data' : '解約済み';
+
+    const isCanceled = subscription_status === CANCELED;
 
     return (
         <div className="bg-form-bg rounded-[20px] xl:max-w-[372px] w-full pt-6 px-5 pb-7 md:px-6">
@@ -25,7 +31,11 @@ const SubscriptionCard = async ({ orderItem }: { orderItem: OrderItem }) => {
             <div className="grid gap-4">
                 <div className="flex justify-center items-baseline flex-wrap gap-1">
                     <p className="text-sm leading-7 font-medium">次回のお支払い日：</p>
-                    <p className="text-sm leading-7 font-medium font-poppins">{formattedNextPaymentDate}</p>
+                    <p className={`text-sm leading-7 font-medium${
+                        isCanceled ? ' text-error' : ' font-poppins'
+                    }`}>
+                        {formattedNextPaymentDate}
+                    </p>
                 </div>
                 <LinkButtonPrimary
                     link={`${SUBSCRIPTION_HISTORY_PATH}/${order_id}`}
