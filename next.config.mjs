a6a3a5f832b3d7cee6788c0291af6b13cc461299
Naoko Kitahara <suisai.port@gmail.com>
2025-10-20 19:49:50 +0900

    [Test] デプロイエラーの検証

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        clientSegmentCache: false,
+    },
     images: {
         remotePatterns: [
             {
