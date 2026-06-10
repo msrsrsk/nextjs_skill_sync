fa71454e6b52868d511f5d5c10822ad5ae88a614
Naoko Kitahara <suisai.port@gmail.com>
2026-06-10 22:06:44 +0900

    [improve] Lottie の表示サイズ修正（TailwindCSS の修正）

--- components/ui/display/LottieAnimation.tsx
+++ components/ui/display/LottieAnimation.tsx
@@ -20,7 +20,7 @@ const LottieAnimation = ({
       src={src}
       autoplay={autoplay}
       loop={loop}
-      className={`w-128 h-128 pointer-events-none ${customClass ?? ""}`}
+      className={`w-[512px] h-[512px] pointer-events-none ${customClass ?? ""}`}
     />
   );
 };
